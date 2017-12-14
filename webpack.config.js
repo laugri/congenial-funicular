84abdbe976b20546b5c4bc7f3390c31f1e4e5a2e
Laurent Grima <grima.laurent@gmail.com>
2017-12-14 18:01:14 +0100

    chore: add basirc webpack config

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+const paths = {
+  output: path.resolve(__dirname, 'build'),
+  source: path.resolve(__dirname, 'src'),
+};
+
+module.exports = {
+  entry: path.join(paths.source, 'app.js'),
+  output: {
+    path: paths.output,
+    filename: 'bundle.js',
+  },
+};
