b6ac16f3b74b04383f83eab5257e5c4955b467a5
Laurent Grima <grima.laurent@gmail.com>
2017-12-15 14:49:34 +0100

    chore: set up webpack dev server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,3 @@
-console.log('Hello World');
+import printMe from './print.js';
+
+printMe();
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,42 @@
 const path = require('path');
+// const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const paths = {
   output: path.resolve(__dirname, 'build'),
   source: path.resolve(__dirname, 'src'),
 };
 
-module.exports = {
+const config = {
   entry: path.join(paths.source, 'app.js'),
   output: {
     path: paths.output,
     filename: 'bundle.js',
   },
+  devtool: 'cheap-module-eval-source-map',
   devServer: {
     contentBase: paths.source,
-    port: 3000,
+    // hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    // new webpack.NamedModulesPlugin(),
+    // new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['babel-preset-env'],
+          },
+        },
+      },
+    ],
   },
 };
+
+module.exports = config;
